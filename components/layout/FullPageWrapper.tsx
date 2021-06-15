3ec3bf6bb9f710fe0906d9f3d373551bb39a1b3a
Cyrille <ngidekollecyrille@gmail.com>
2021-06-15 22:11:06 +0200

    Add contact form

--- components/layout/FullPageWrapper.tsx
+++ components/layout/FullPageWrapper.tsx
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from "react";
 import tw from "twin.macro";
 import styled, { css } from "styled-components";
-import { motion } from "framer-motion";
 
 type PageProps = {};
 
 const Wrapper = styled.div<{ right?: boolean; animate: boolean }>`
-  ${tw`bg-gray-300 flex flex-col min-h-screen pt-12 lg:`}/* animation: 2s ease-out 1s 1 slideInFromLeft;
-  @keyframes slideInFromLeft {
-    0% {
-      transform: translateY(-1%);
-    }
-    100% {
-      transform: translateY(0);
-    }
-  } */
+  ${tw`bg-gray-300 flex flex-col min-h-screen pt-12 lg:`}
+  position: relative;
 `;
 const InnerWrapper = styled.div`
   ${tw`self-center mx-5`}
   max-width: 1080px;
+  position: relative;
 `;
 
 export const FullPageWrapper: React.FC<PageProps> = (props) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FullPageWrapper } from "../components/layout/FullPageWrapper";
 import { LeftContent } from "../components/layout/LeftContent";
 import { RightContent } from "../components/layout/RightContent";
@@ -14,9 +14,9 @@ import { SvgContent } from "../components/layout/SvgContent";
 import { IntroContainer } from "../components/layout/IntroContainer";
 import { SegmentContainer } from "../components/layout/SegmentContainer";
 import { Footer } from "../components/layout/Footer";
-
+import Modal from "../components/layout/Modal";
 import tw from "twin.macro";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 const Row = styled.div`
   ${tw`flex flex-col`}
@@ -26,124 +26,150 @@ const Row = styled.div`
 `;
 
 export default function Home() {
-  return (
-    <FullPageWrapper>
-      <HeaderContent
-        name={"Cyrille Ngide Kolle"}
-        description={"Frontend och backend-utvecklare"}
-      />
-      <Divider marginTop={false} />
-      <Row>
-        <LeftContent>
-          <IntroContainer>
-            <ImageContent />
-            <ContactContent
-              title="Kontakt"
-              email="ngidekollecyrille@gmail.com"
-              linkedin="LinkedIn"
-              tel="+46(0)737-66 82 28"
-              github="Github"
-            />
-          </IntroContainer>
-        </LeftContent>
-        <RightContent>
-          <Divider largeHiden={false} midHidden={false} />
-          <IntroContent
-            reference={"Eric Ellersten, Kreativt ansvarig på Itiden:"}
-            mainText={"”Han är positiv, intresserad och mycket trevlig”"}
-            description={
-              "Att bli programmerare är ett av de bästa besluten jag någonsin har tagit. Att ständigt få bli utmanad och hitta nya lösningar, gör att jobbet alltid är spännande. Jag är en ödmjuk och ambitiös kille med väldig stor passion för programmering. Under dem senaste månaderna har jag jobbat som praktikant hos itiden AB, där jag har arbetat med en mängdt olika projekt byggt med react, typescript, javascript, nextjs, för att nämna några. Men har också jobbat agilt och enligt olika design. Jag är även intresserad att lära mig nya språk och tekniker som .Net, laravel, php, python, cms, wordpress, adonis."
-            }
-          />
-          <Divider hidden={false} largeHiden={false} midHidden={false} />
-          <SegmentContainer>
-            <SecondContent
-              title={"Arbetslivserfarenhet"}
-              primaryItem1={"Itiden AB (LIA)"}
-              primaryItem2={"Dagab"}
-              primaryItem3={"Frode Laursen"}
-              primaryItem4={"Postnord terminalen"}
-              secondaryItem1={"Praktikant"}
-              secondaryItem2={"Lagerarbetare"}
-              secondaryItem3={"Lagerarbetare"}
-              secondaryItem4={"Largerarbetare"}
-              tertiaryItem1={"Feb 2021–Jun 2021"}
-              tertiaryItem2={"2020–Pågående"}
-              tertiaryItem3={"2018–2020"}
-              tertiaryItem4={"2018–2019"}
-            />
-            <Divider hidden={false} largeHiden={false} midHidden={false} />
-            <SecondContent
-              marginLeft={false}
-              maxWidth={false}
-              title={"Utbildningar"}
-              primaryItem1={"It-Högskolan"}
-              primaryItem2={"University of Buea"}
-              primaryItem3={"Lycée Bilingue Buea"}
-              secondaryItem1={"Applikationsutvecklare"}
-              secondaryItem2={"Accounting"}
-              secondaryItem3={"Certificate, 2006–2008 "}
-              tertiaryItem1={"Göteborg, 2019–2021"}
-              tertiaryItem2={"Buea, Cameroon, 2008–2011"}
-              tertiaryItem3={"Buea, Cameroon, 2006–2008"}
-            />
-          </SegmentContainer>
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const handleScroll = () => {
+    const bottom =
+      Math.ceil(window.innerHeight + window.scrollY) >=
+      document.documentElement.scrollHeight;
 
-          <Divider />
-          <CompetencyContent
-            title={"Kompetenser"}
-            competencies={[
-              "Javascript",
-              "Java",
-              "React",
-              "Responsive Designn",
-              "Typescript",
-              "Swift",
-              "Objective-C",
-              "SQL",
-              "Figma",
-              "Next js",
-              "Tailwind",
-              "Styled Components",
-              "git",
-              "Terminal",
-              "Android Studio",
-              "Xcode",
-              "Firebase",
-              "HTML",
-              "CSS",
-              "React Native",
-              "Vue.js",
-              "Agile methodologies",
-              "Scrum",
-              "Project Management",
-            ]}
-          />
-          <Divider hidden={false} largeHiden={false} midHidden={false} />
-          <SegmentContainer>
-            <SecondContent
-              title={"Språkkunskaper"}
-              primaryItem1={"Engelska"}
-              primaryItem2={"Franska"}
-              primaryItem3={"Svenska"}
-              secondaryItem1={"Skriver: Flytande"}
-              secondaryItem2={"Skriver: Väldigt bra"}
-              secondaryItem3={"Skriver: Väldigt bra"}
-              tertiaryItem1={"Talar: Flytande"}
-              tertiaryItem2={"Talar: Flytande"}
-              tertiaryItem3={"Talar: Väldigt bra"}
+    if (bottom) {
+      setTimeout(() => {
+        setIsModalVisible(true);
+      }, 400);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, {
+      passive: true,
+    });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <FullPageWrapper>
+        <HeaderContent
+          name={"Cyrille Ngide Kolle"}
+          description={"Frontend och backend-utvecklare"}
+        />
+        <Divider marginTop={false} />
+        <Row>
+          <LeftContent>
+            <IntroContainer>
+              <ImageContent />
+              <ContactContent
+                title="Kontakt"
+                email="ngidekollecyrille@gmail.com"
+                linkedin="LinkedIn"
+                tel="+46(0)737-66 82 28"
+                github="Github"
+              />
+            </IntroContainer>
+          </LeftContent>
+          <RightContent>
+            <Divider largeHiden={false} midHidden={false} />
+            <IntroContent
+              reference={"Eric Ellersten, Kreativt ansvarig på Itiden:"}
+              mainText={"”Han är positiv, intresserad och mycket trevlig”"}
+              description={
+                "Att bli programmerare är ett av de bästa besluten jag någonsin har tagit. Att ständigt få bli utmanad och hitta nya lösningar, gör att jobbet alltid är spännande. Jag är en ödmjuk och ambitiös kille med väldig stor passion för programmering. Under dem senaste månaderna har jag jobbat som praktikant hos itiden AB, där jag har arbetat med en mängdt olika projekt byggt med react, typescript, javascript, nextjs, för att nämna några. Men har också jobbat agilt och enligt olika design. Jag är även intresserad att lära mig nya språk och tekniker som .Net, laravel, php, python, cms, wordpress, adonis."
+              }
             />
             <Divider hidden={false} largeHiden={false} midHidden={false} />
-            <ReferenceContent
-              title={"Referenser"}
-              description={"Referenser lämnas på begäran"}
+            <SegmentContainer>
+              <SecondContent
+                title={"Arbetslivserfarenhet"}
+                primaryItem1={"Itiden AB (LIA)"}
+                primaryItem2={"Dagab"}
+                primaryItem3={"Frode Laursen"}
+                primaryItem4={"Postnord terminalen"}
+                secondaryItem1={"Praktikant"}
+                secondaryItem2={"Lagerarbetare"}
+                secondaryItem3={"Lagerarbetare"}
+                secondaryItem4={"Largerarbetare"}
+                tertiaryItem1={"Feb 2021–Jun 2021"}
+                tertiaryItem2={"2020–Pågående"}
+                tertiaryItem3={"2018–2020"}
+                tertiaryItem4={"2018–2019"}
+              />
+              <Divider hidden={false} largeHiden={false} midHidden={false} />
+              <SecondContent
+                marginLeft={false}
+                maxWidth={false}
+                title={"Utbildningar"}
+                primaryItem1={"It-Högskolan"}
+                primaryItem2={"University of Buea"}
+                primaryItem3={"Lycée Bilingue Buea"}
+                secondaryItem1={"Applikationsutvecklare"}
+                secondaryItem2={"Accounting"}
+                secondaryItem3={"Certificate, 2006–2008 "}
+                tertiaryItem1={"Göteborg, 2019–2021"}
+                tertiaryItem2={"Buea, Cameroon, 2008–2011"}
+                tertiaryItem3={"Buea, Cameroon, 2006–2008"}
+              />
+            </SegmentContainer>
+
+            <Divider />
+            <CompetencyContent
+              title={"Kompetenser"}
+              competencies={[
+                "Javascript",
+                "Java",
+                "React",
+                "Responsive Designn",
+                "Typescript",
+                "Swift",
+                "Objective-C",
+                "SQL",
+                "Figma",
+                "Next js",
+                "Tailwind",
+                "Styled Components",
+                "git",
+                "Terminal",
+                "Android Studio",
+                "Xcode",
+                "Firebase",
+                "HTML",
+                "CSS",
+                "React Native",
+                "Vue.js",
+                "Agile methodologies",
+                "Scrum",
+                "Project Management",
+              ]}
             />
-          </SegmentContainer>
-        </RightContent>
-      </Row>
-      <SvgContent />
-      <Divider bottomMargin={false} marginTop={false} />
-      <Footer />
-    </FullPageWrapper>
+            <Divider hidden={false} largeHiden={false} midHidden={false} />
+            <SegmentContainer>
+              <SecondContent
+                title={"Språkkunskaper"}
+                primaryItem1={"Engelska"}
+                primaryItem2={"Franska"}
+                primaryItem3={"Svenska"}
+                secondaryItem1={"Skriver: Flytande"}
+                secondaryItem2={"Skriver: Väldigt bra"}
+                secondaryItem3={"Skriver: Väldigt bra"}
+                tertiaryItem1={"Talar: Flytande"}
+                tertiaryItem2={"Talar: Flytande"}
+                tertiaryItem3={"Talar: Väldigt bra"}
+              />
+              <Divider hidden={false} largeHiden={false} midHidden={false} />
+              <ReferenceContent
+                title={"Referenser"}
+                description={"Referenser lämnas på begäran"}
+              />
+            </SegmentContainer>
+          </RightContent>
+        </Row>
+        <SvgContent />
+        <Divider bottomMargin={false} marginTop={false} />
+        {isModalVisible === true && <Modal />}
+        <Footer />
+      </FullPageWrapper>
+    </>
   );
 }
