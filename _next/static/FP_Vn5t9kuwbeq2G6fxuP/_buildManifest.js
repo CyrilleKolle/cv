420e7ba6a3ecd10782489d3276f2052cad158196
CyrilleKolle <56865654+CyrilleKolle@users.noreply.github.com>
2021-07-26 18:31:30 +0000

    Deploying to gh-pages from @ CyrilleKolle/cv@336a0caab5e07526c5b63606e6fc5071ec9e981b 🚀

---
+++ _next/static/FP_Vn5t9kuwbeq2G6fxuP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-52599e735155f3f3a4e3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8a5984dee087f239da70.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
